a8a6179ea5feec099c12b255d3aa27cf585432fe
RauloGti <raulogtis@gmail.com>
2023-05-11 18:42:11 -0300

    prrimer parte completada trabajo integrador proyecto_Listas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { AlertController, ToastController } from '@ionic/angular';
+import { ListaService } from '../services/lista.service';
+import { Lista } from '../models/lista.model';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,103 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public alertController:AlertController,
+    public toastController:ToastController,
+    public listaService:ListaService) {}
+  /**
+ * @function AgregarLista
+ * @description La función será ejecutada cuando el usuario haga click en el botón Agregar
+ * Muestra una alerta donde solicita el nombre de la lista
+ */
 
-}
+    async AgregarLista() {
+      let alerta = await this.alertController.create({
+      header: "Agregar lista",
+      inputs: [
+      {
+      type: "text",
+      name: "titulo",
+      placeholder: "Ingresar nombre de la lista"
+      }
+      ],
+      buttons: [
+        {
+        text: "Cancelar",
+        role: "cancel"
+        },
+        {
+        text: "Crear",
+        handler: (data:any)=> {
+          let esValido: boolean = this.validarInput(data);
+          if (esValido){
+            let creadaOk = this.listaService.crearLista(data.titulo);
+            if(creadaOk) { //Se verifica si la variable tiene un valor, es decir, que fue creada
+            this.presentToast('Lista creada correctamente!');
+            }
+          }
+        }
+        }
+      ] 
+      })
+      await alerta.present();
+      console.log('Hola Mundo!');
+    }
+    
+    async EditarLista(lista: Lista) {
+      let alerta = await this.alertController.create({
+      header: "Editar lista",
+      inputs: [
+      {
+      type: "text",
+      name: "titulo",
+      placeholder: "Ingresar nuevo nombre de la lista",
+      value: lista.titulo
+      }
+      ],
+      buttons: [
+      {
+      text: "Cancelar",
+      role: "cancel"
+      },
+      {
+        text: "Editar",
+        handler: (data:any)=> {
+        let esValido: boolean = this.validarInput(data);
+        if (esValido){
+        lista.titulo = data.titulo,
+        this.listaService.editarLista(lista); 
+        this.presentToast('Lista editada correctamente!'); 
+        } 
+        }
+        }
+        ]
+        })
+        await alerta.present(); 
+        }
+       
+  validarInput(input: any):boolean {
+    if(input && input.titulo) {
+    return true;
+  }
+  this.presentToast("Debe Ingresar un Valor");
+   return false;
+  }
+  async presentToast(mensage:string) {
+    let toast = await this.toastController.create({
+    message: mensage,
+    duration: 2000
+    });
+    toast.present();
+    }
+    
+    editarLista(listaItem: Lista) {
+      //this.EditarLista(listaItem);
+      console.log("Editar lista:",listaItem);
+      }
+    eliminarLista(listaItem: Lista) {
+      //this.listaService.eliminarLista(listaItem);
+      console.log("Eliminar lista:",listaItem);
+      }
+    
+  }    
